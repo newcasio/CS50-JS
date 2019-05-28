86c31a62f1b2325524ea46beced6039c71534fdf
Brad <braddong@hotmail.com>
2019-05-28 20:48:46 +1000

    harder mario done

--- mario/mario.js
+++ mario/mario.js
@@ -7,3 +7,24 @@ while (height > 8 || height < 1 || isNaN(height)) {
   height = prompt("what is the height?");
   x = parseInt(height);
 }
+
+//layers
+for (let i = 1; i <= x; i++) {
+  //   debugger;
+  let counter;
+  let arr = [];
+  for (let j = 0; j < x - i; j++) {
+    arr.push(" ");
+  }
+  for (let j = 0; j < i; j++) {
+    arr.push("#");
+  }
+  arr.push(" ");
+  for (let j = 0; j < i; j++) {
+    arr.push("#");
+  }
+  for (let j = 0; j < x - i; j++) {
+    arr.push(" ");
+  }
+  console.log(arr.join(""));
+}
